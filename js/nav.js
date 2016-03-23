8d1540420d102cf666f9a953144fe65d22d841e7
Milo Wyner <milowyner@gmail.com>
2016-03-23 12:59:27 -0700

    Comment plan in nav.js

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+//Promlem: Nav links don't have animation when clicked
+//Solution: Show nested nav smoothly when clicked
+
+//1. When clicked user clicks nav link
+	//1.1 Show nested nav with animation
+	//1.2 When user clicks nav link again
+		//1.2.1 Go to link
+//2. When user clicks on another nav link
+	//2.1 Hide other nested navs
+	//2.2 Show nested nav of nav link clicked
