9490b8909b3e4847e0db959846686409956cd7cc
Milo Wyner <milowyner@gmail.com>
2016-03-23 14:29:08 -0700

    Update nav.js
    Made it so when you click on a nav link, the nested nav for that link
    shows, then when you click on the nav link again, it will go to the
    page linked.

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,18 @@
 //Promlem: Nav links don't have animation when clicked
 //Solution: Show nested nav smoothly when clicked
 
-//1. Disable showing of nav link on hover
-//2. When clicked user clicks nav link
-	//2.1 Show nested nav with animation
-	//2.2 When user clicks nav link again
-		//2.2.1 Go to link
-//3. When user clicks on another nav link
-	//3.1 Hide other nested navs
-	//3.2 Show nested nav of nav link clicked
+// Prevent nav link from going to linked page
+$('.nav-link > a').bind('click', function(e){
+        e.preventDefault();
+});
+// When user clicks nav link
+$('.nav-link > a').click(function () {
+	// Show nested nav, with animation
+	$(this).next().slideDown(200);
+	// Make link clickable again
+	$(this).unbind('click');
+});
+
+// When user clicks on another nav link
+	// Hide other nested navs
+	// Show nested nav of nav link clicked
