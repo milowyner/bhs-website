9e66fb8a53eaaba17eb760df930bed9c530b5e3b
Milo Wyner <milowyner@gmail.com>
2016-03-23 13:00:29 -0700

    Update plan in nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,11 @@
 //Promlem: Nav links don't have animation when clicked
 //Solution: Show nested nav smoothly when clicked
 
-//1. When clicked user clicks nav link
-	//1.1 Show nested nav with animation
-	//1.2 When user clicks nav link again
-		//1.2.1 Go to link
-//2. When user clicks on another nav link
-	//2.1 Hide other nested navs
-	//2.2 Show nested nav of nav link clicked
+//1. Disable showing of nav link on hover
+//2. When clicked user clicks nav link
+	//2.1 Show nested nav with animation
+	//2.2 When user clicks nav link again
+		//2.2.1 Go to link
+//3. When user clicks on another nav link
+	//3.1 Hide other nested navs
+	//3.2 Show nested nav of nav link clicked
