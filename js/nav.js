2b38f090a086de6dac3f12cb553b0538c5c93450
Milo Wyner <milowyner@gmail.com>
2016-03-25 18:42:45 -0700

    Update nav.js
    Made it so when you click/tap on a nav link and the nested nav opens,
    the color of the nav link will change to orange. When you click on it
    again or click on another nav link, it will turn white again.

--- js/nav.js
+++ js/nav.js
@@ -6,14 +6,17 @@ var openNav;
 $('.nav-link > a').click(function () {
 	// Hide all open nested navs (which should just be the last opened nav)
 	$('.nav-link > ul').slideUp(300);
+	// Change all nav link colors to white (should also just be last)
+	$('.nav-link > a').css('color', '#fff');
 	// If nested nav is open when clicked
 	if (openNav == this.innerText) {
 		$(this).next().slideUp(300); // Hide nested nav
+		$(this).css('color', '#fff'); // Change nav link color to white
 		openNav = null;
-		$(this).setAttr(color, '#fff');
 	// If the nested nav is closed when clicked
 	} else {
 		$(this).next().slideDown(300); // Show nested nav
+		$(this).css('color', '#f60'); // Change nav link color to orange
 		openNav = this.innerText;
 	}
 	return false;
