df1617cf53f96d6f59209c0dc43a7e531e4da8c7
Milo Wyner <milowyner@gmail.com>
2016-03-25 18:05:49 -0700

    Update nav functionality and rename .js files

--- js/nav.js
+++ js/nav.js
@@ -1,23 +1,19 @@
-// ALTERNATE Nav.js
+// Nav.js
 // Shows nested navs smoothly when main nav links are clicked
-// ALSO: Allows user to click once on a nav link to open nested nav, then click again on the same 
-// 		 nav link to open external link
 
-function preventLink (e) {
-	e.preventDefault();
-}
-
-// Prevent nav link from going to linked page
-$('.nav-link > a').bind('click', preventLink);
-
+var openNav;
 // When user clicks nav link
 $('.nav-link > a').click(function () {
-	// Reset other nav links and nested navs
-	$('.nav-link > ul').slideUp(300); // Hide open nested navs, with animation
-	$('.nav-link > a').removeClass("arrow-after"); // Remove arrows
-	$('.nav-link > a').bind('click', preventLink); // Prevent links from activating
-	// Open/animate clicked nav link
-	$(this).next().slideDown(300); // Show nested nav, with animation
-	$(this).addClass("arrow-after"); // Add arrow to side of nav link
-	$(this).unbind('click', preventLink); // Make nav link clickable
+	// Hide all open nested navs (which should just be the last opened nav)
+	$('.nav-link > ul').slideUp(300);
+	// If nested nav is open when clicked
+	if (openNav == this.innerText) {
+		$(this).next().slideUp(300); // Hide nested nav
+		openNav = null;
+	// If the nested nav is closed when clicked
+	} else {
+		$(this).next().slideDown(300); // Show nested nav
+		openNav = this.innerText;
+	}
+	return false;
 });
