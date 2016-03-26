b5e6365f85f2edda5cbc9d3379e0e7b9649fa3e9
Milo Wyner <milowyner@gmail.com>
2016-03-25 18:03:34 -0700

    Testing different nav functionality

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,7 @@
-// Nav.js
+// ALTERNATE Nav.js
 // Shows nested navs smoothly when main nav links are clicked
+// ALSO: Allows user to click once on a nav link to open nested nav, then click again on the same 
+// 		 nav link to open external link
 
 function preventLink (e) {
 	e.preventDefault();
@@ -11,11 +13,11 @@ $('.nav-link > a').bind('click', preventLink);
 // When user clicks nav link
 $('.nav-link > a').click(function () {
 	// Reset other nav links and nested navs
-	$('.nav-link > ul').slideUp(200); // Hide open nested navs, with animation
+	$('.nav-link > ul').slideUp(300); // Hide open nested navs, with animation
 	$('.nav-link > a').removeClass("arrow-after"); // Remove arrows
 	$('.nav-link > a').bind('click', preventLink); // Prevent links from activating
 	// Open/animate clicked nav link
-	$(this).next().slideDown(200); // Show nested nav, with animation
+	$(this).next().slideDown(300); // Show nested nav, with animation
 	$(this).addClass("arrow-after"); // Add arrow to side of nav link
 	$(this).unbind('click', preventLink); // Make nav link clickable
 });
