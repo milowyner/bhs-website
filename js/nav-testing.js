1a48236e386ab7606bba0984cb6682c3dc95640d
Milo Wyner <milowyner@gmail.com>
2016-03-27 17:49:30 -0700

    Update navigation

--- js/nav-testing.js
+++ js/nav-testing.js
@@ -1,9 +1,20 @@
 // Nav.js
 // Shows nested navs smoothly when main nav links are clicked
 
-// Disable show on hover
+// Disable show on hover from css
+$('body').removeClass('no-javascript');
+
+// Show nested nav on hover
 $('.nav-link').hover(function() {
 	$(this).children('ul').slideDown(300);
+	$(this).children('a').css('color', '#f60');
 }, function() {
 	$(this).children('ul').slideUp(300);
+	$(this).children('a').css('color', '#fff');
+});
+
+// Hide nested nav on click
+$('.nav-link').click(function() {
+	$(this).children('ul').slideUp(300);
+	$(this).children('a').css('color', '#fff');
 });
