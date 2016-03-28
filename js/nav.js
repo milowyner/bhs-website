1c417c7dc7f1177edc166a3025ece23d5e0897bf
Milo Wyner <milowyner@gmail.com>
2016-03-28 00:54:39 -0700

    Finished nav that works on mobile and desktop

--- js/nav.js
+++ js/nav.js
@@ -25,24 +25,3 @@ $('.nav-link > a').click(function() {
 	}
 	return false;
 });
-
-// TESTING
-
-function clickToggle() {
-	// Hide all open nested navs (which should just be the last opened nav)
-	$('.nav-link ul').slideUp(300);
-	// Change all nav link colors to white (should also just be last)
-	$('.nav-link > a').css('color', '#fff');
-	// If nested nav is open when clicked
-	if (openNav == this) {
-		$(this).next().slideUp(300); // Hide nested nav
-		$(this).css('color', '#fff'); // Change nav link color to white
-		openNav = null;
-	// If the nested nav is closed when clicked
-	} else {
-		$(this).next().slideDown(300); // Show nested nav
-		$(this).css('color', '#f60'); // Change nav link color to orange
-		openNav = this;
-	}
-	return false;
-}
