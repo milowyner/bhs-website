2082889da43ae9bbddf6792bbeed56773dae30b5
Milo Wyner <milowyner@gmail.com>
2016-03-27 21:51:25 -0700

    Testing nav stuff

--- js/nav-testing.js
+++ js/nav-testing.js
@@ -4,17 +4,28 @@
 // Disable show on hover from css
 $('body').removeClass('no-javascript');
 
+$(document).ready(function() {
+    // run test on initial page load
+    checkSize();
+
+    // run test on resize of the window
+    $(window).resize(checkSize);
+});
+
+//Function to the css rule
+function checkSize(){
+    if ($(".main-nav > ul").css("display") == "block" ){
+        $('.nav-link').click(function() {
+        	$(this).children('ul').slideToggle(300);
+        })
+    }
+}
+
 // Show nested nav on hover
 $('.nav-link').hover(function() {
 	$(this).children('ul').slideDown(300);
 	$(this).children('a').css('color', '#f60');
 }, function() {
-	$(this).children('ul').slideUp(300);
-	$(this).children('a').css('color', '#fff');
-});
-
-// Hide nested nav on click
-$('.nav-link').click(function() {
 	$(this).children('ul').slideUp(300);
 	$(this).children('a').css('color', '#fff');
 });
--- js/nav.js
+++ js/nav.js
@@ -2,28 +2,26 @@
 // Shows nested navs smoothly when main nav links are clicked
 
 var openNav;
+
+// Disable show on hover from css
+$('body').removeClass('no-javascript');
+
 // When user clicks nav link
 $('.nav-link > a').click(function () {
 	// Hide all open nested navs (which should just be the last opened nav)
-	$('.nav-link > ul').slideUp(300);
+	$('.nav-link ul').slideUp(300);
 	// Change all nav link colors to white (should also just be last)
 	$('.nav-link > a').css('color', '#fff');
-	if ($('.nav-link > a').hasClass('selected')) { // Keeps .selected nav links colored orange
-		$('.selected').css('color', '#f60');
-	}
 	// If nested nav is open when clicked
-	if (openNav == this.innerText) {
+	if (openNav == this) {
 		$(this).next().slideUp(300); // Hide nested nav
 		$(this).css('color', '#fff'); // Change nav link color to white
-		if ($('.nav-link > a').hasClass('selected')) { // Keeps .selected nav links colored orange
-			$('.selected').css('color', '#f60');
-		}
 		openNav = null;
 	// If the nested nav is closed when clicked
 	} else {
 		$(this).next().slideDown(300); // Show nested nav
 		$(this).css('color', '#f60'); // Change nav link color to orange
-		openNav = this.innerText;
+		openNav = this;
 	}
 	return false;
 });
